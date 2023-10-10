12bb510ca97a6e234e59a4ea1e58351aa349c0e5
Dell-User <Dell-User@DESKTOP-GCMBBUK>
2023-10-10 09:00:06 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim counter, sum As Integer
+        For counter = 1 To 100 Step 10
+            sum += counter
+            ListBox1.Items.Add(sum)
+        Next
+    End Sub
+End Class
